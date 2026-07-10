3d2665dd17dedc10651da860e1f07c6d74cbeeaa
kush2501 <coder8342@gmail.com>
2026-07-10 16:02:50 +0530

    Add experiment 1 - LOR Baseline - LRegression

---
+++ notebooks/dagshub_tracking.py
@@ -0,0 +1,9 @@
+import mlflow
+import dagshub
+dagshub.init(repo_owner='kush2501', repo_name='mlops-mini-project', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/kush2501/mlops-mini-project.mlflow")
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
